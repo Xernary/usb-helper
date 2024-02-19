0527bf8f99f237407389c32a67d5504844beaf59
Xernary <nicolalggiuffrida@gmail.com>
2024-02-19 19:09:59 +0100

    more stuff

--- usb-helper.c
+++ usb-helper.c
@@ -3,8 +3,16 @@
 #include <string.h>
 
 
-const char* const path = "/dev";
+const char* const path = "/dev/disk/by-path";
+const char* const substring = "usb";
+int current_usbs = 0;
 
+// letter b/c in sdb/sda is a counter
+// so first disk is sda, second sdb and so on
+
+// MAIN DISK name is sda 
+// SECOND DISK name sdb is from [sda to sde] and partitions [sda1 to sda9] 
+// USB name is sdb
 
 // creates heap with n elements and shows it before and after heapify
 void test_heap(int n){
@@ -23,10 +31,24 @@ int filter(const struct dirent* dirent){
   int select = 0;
   const char* name = dirent->d_name;
 
-  if(dirent->d_type == DT_BLK && strcmp(name, ".") != 0 && strcmp(name, "..") != 0)
+  if(strstr(name, substring) != NULL && dirent->d_type == DT_LNK && 
+     strcmp(name, ".") != 0 && strcmp(name, "..") != 0)
     select = 1;
 
   return select;
+}
+
+
+
+int get_usbs_number(){
+  struct dirent** dirents_list;
+  int n;
+  n = scandir(path, &dirents_list, filter, alphasort);
+  for(int i = 0; i < n; i++)
+    free(dirents_list[i]);
+  free(dirents_list);
+
+  return n;
 }
 
 
@@ -39,17 +61,17 @@ int get_names_list(char*** names){
   char** names_list;
 
   n = scandir(path, &dirents_list, filter, alphasort);
-  if(n == -1){
+  if(n <= 0){
     perror("Error in scandir");
     exit(1);
   }
 
-  names_list = malloc(sizeof(char) * n);
+  names_list = malloc(sizeof(char*) * n);
 
   for(int i = 0; i < n; i++){
-    //printf("%s\n", dirents_list[i]->d_name);
+    printf("%s\n", dirents_list[i]->d_name);
 
-    names_list[i] = malloc(sizeof(char) * strlen(dirents_list[i]->d_name));
+    names_list[i] = malloc(sizeof(char) * strlen(dirents_list[i]->d_name) + 1);
     strcpy(names_list[i], dirents_list[i]->d_name); 
 
     free(dirents_list[i]);
@@ -63,18 +85,31 @@ int get_names_list(char*** names){
 
 
 
+void start_detector(){
+  int n;
+  while(1){
+    if((n = get_usbs_number()) != current_usbs){
+      printf("%s", "[+] new usb detected\n");
+      // handle the new usb
+      current_usbs = n;
+    }
+  }
+}
+
+
 int main(){
 
   int fd;
   char** names_list;  
-  int n;
   test_heap(10);
 
-  n = get_names_list(&names_list); 
+  current_usbs = get_names_list(&names_list); 
 
-  printf("n: %d\n\n", n);
+  printf("n: %d\n\n", current_usbs);
 
-  for(int i = 0; i < n; i++){
+  //start_detector();
+
+  for(int i = 0; i < current_usbs; i++){
     printf("%s\n", names_list[i]);
     free(names_list[i]);
   }
